7769ea3879f8cb3a0e74aaca02619cfe1fdd601f
gamedari <godreigns.amedari@pau.edu.ng>
2025-10-12 23:48:18 +0100

    (Added compiled files)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -1,12 +1,10 @@
 fn main() {
-    // Given values
-    let principal = 520_000_000.0; 
-    let rate = 10.0;               
-    let years = 5.0;               
+	let p:f64 = 520_000_000.00; 
+	let r:f64 = 10.0;
+	let t:f64 = 5.0;
 
-    let amount = principal * (1.0 + (rate / 100.0)).powf(years);
-
-    let compound_interest = amount - principal;
-
-    println!("Total Amount after 5 years: ₦{:.2}", amount);
-    println!("Compound Interest: ₦{:.2}", compound_interest);
+	let ar = 1.0 + (r / 100.0);
+	let am = ar.powf(t) * p;
+	let ci = am - p;
+	print!("The compound interest is ₦{:.2}", ci);
+}
--- week-2/project_3.rs
+++ week-2/project_3.rs
@@ -1,8 +1,8 @@
 fn main() {
     // Given values
-    let principal = 510_000.0; 
-    let rate = 5.0;           
-    let years = 3.0;         
+    let principal:f64 = 510_000.0; 
+    let rate:f64 = 5.0;           
+    let years:f64 = 3.0;
 
     let amount = principal * (1.0 - (rate / 100.0)).powf(years);
 
