3b38b72ec300e788741ed788147fc9748d6ef79d
gamedari <godreigns.amedari@pau.edu.ng>
2025-11-09 22:24:55 +0100

    (deleted something)

--- test/practice/target/debug/deps/practice.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Hp\Documents\g.amedariCOS101\test\practice\target\debug\deps\practice.d: src\main.rs
-
-C:\Users\Hp\Documents\g.amedariCOS101\test\practice\target\debug\deps\practice.exe: src\main.rs
-
-src\main.rs:
--- test/practice/target/debug/practice.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Hp\Documents\g.amedariCOS101\test\practice\target\debug\practice.exe: C:\Users\Hp\Documents\g.amedariCOS101\test\practice\src\main.rs
