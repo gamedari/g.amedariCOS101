edbffeafb490d3ba9044258c6ea5b23b84aaac36
gamedari <godreigns.amedari@pau.edu.ng>
2025-11-19 13:32:37 +0100

    (initialzing and accesing elements and passing tuple as parameter

--- week-8/practice_8/src/main.rs
+++ week-8/practice_8/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // initialize a mutable tuple
+    let mut mountain_heights = ("Everest", 8848, "Fishtail", 6693);
+
+    println!("Original tuple = {:?}", mountain_heights);
+
+    // chnage 3rd and 4th element of a mutable tuple
+    mountain_heights.2 = "Lhotse";
+    mountain_heights.3 = 8516;
+
+    println!("Changed tuple = {:?}", mountain_heights);
 }
