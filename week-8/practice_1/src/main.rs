681f2a810eafb33cfb49baa7dad8a65ca46bf8e1
gamedari <godreigns.amedari@pau.edu.ng>
2025-11-19 13:03:01 +0100

    (creating and accessing a vector)

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let v : Vec<i64> = Vec::new();
+
+    println!("(\nThe length of VecLLnew is: {}", v.len());
+
+    let v = vec!["Grace", "Effiong", "Basil", "Kareem", "Suasn"];
+
+    // printning the isze of vector
+    println!("\nThe length of vec macro is: {}", v.len());
+}
