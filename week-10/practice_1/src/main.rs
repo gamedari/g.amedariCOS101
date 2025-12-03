ceb2bf9997b3957c0cce64ca1132888247b880c1
gamedari <godreigns.amedari@pau.edu.ng>
2025-12-03 13:45:28 +0100

    (assigned value of one variable to another)

---
+++ week-10/practice_1/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+   let v = vec![101, 205, 330, 400];
+
+   let v2 = &v;
+   println!("{:?}", v);
+}
---
+++ week-10/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\Hp\Documents\g.amedariCOS101\week-10\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\Hp\Documents\g.amedariCOS101\week-10\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
